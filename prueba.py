8c2a01135256ef38749bf3dd78f1f41e8aa0554f
Alex <alex.m.guinea@alu.comillas.edu>
2022-12-01 16:03:24 +0100

    main

--- prueba.py
+++ prueba.py
@@ -1,3 +1,37 @@
-import pandas as pd
+import subprocess
+import platform as pl  #  obtemer informacion del nodo
+import re
 
-print(pd.to_datetime('today'))
+def detalles_nodo_actual():
+    response = {
+        'maquina': pl.machine(),
+        'nombre_sistema': pl.system(),
+        'version': pl.version(),
+    }
+    return response
+
+system = detalles_nodo_actual()['nombre_sistema']
+
+if system == 'Windows':
+    batcmd = "ipconfig"
+    result = subprocess.check_output(batcmd, shell=True)
+    with open("prueba.txt", "wb") as f:
+        f.write(result)
+    result = str(result)
+
+    ini = re.search('Ethernet:', result)
+    b = re.search('\d+\.\d+\.\d+\.\d+', result[ini.start():])
+
+    print(b.group())
+
+elif system == 'Linux':
+    batcmd = "ifconfig"
+    result = subprocess.check_output(batcmd, shell=True)
+    with open("prueba.txt", "wb") as f:
+        f.write(result)
+    result = str(result)
+
+    ini = re.search('enp0s3|eth2:', result)
+    b = re.search('\d+\.\d+\.\d+\.\d+', result[ini.start():])
+
+    print(b.group())
